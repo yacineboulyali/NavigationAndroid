1770dac70d6459c1baa338d6b84515a1ec00b7bc
yacineboulyali <yacineboulyalis@gmail.com>
2018-10-09 20:14:48 +0100

    firsta

---
+++ app/src/main/java/com/example/yacine/nav/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.example.yacine.nav
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        go_to_account.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.to_accountFragment))
+
+    }
+
+}
